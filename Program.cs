59790e5efe10322a6e727fc4a9193651aa75c41b
Inesa Kostova <kostovainesa@gmail.com>
2025-02-03 13:07:50 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿Console.WriteLine(1);
-Console.WriteLine(2);
-Console.WriteLine(3);
-Console.WriteLine(4);
-Console.WriteLine(5);
-Console.WriteLine(6);
-Console.WriteLine(7);
-Console.WriteLine(8);
-Console.WriteLine(9);
-Console.WriteLine(10);
+﻿int SideA = int.Parse(Console.ReadLine());
+int SideB = int.Parse(Console.ReadLine());
+int Area = SideA * SideB;
+Console.WriteLine(Area);
